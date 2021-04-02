1693a92dcf089d55d167fe794c397ffe0b859119
istvan-halasz <haistvan@gmail.com>
2021-04-02 18:45:30 +0300

    Adding Basic REST Routes

--- app.js
+++ app.js
@@ -1,23 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const locationRoutes = require('./routes/location');
+
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+//app.set('view engine', 'ejs');
+//app.set('views', 'views');
 
-app.use(bodyParser.urlencoded({ extended: false })); //body is parsed into properties
+//app.use(bodyParser.urlencoded({ extended: false })); //body is parsed into properties
 
-app.use((req, res, next) => {
-  res.setHeader('Content-Type', 'text/html');
-  next(); //goes to the next middleware, doesn't return a response for the request
-});
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  const userName = req.body.username || 'Unknown User'; //req.body.username from the parsed body
-  res.render('index', { //index file from views folder
-    user: userName
-  });
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
 });
+
+app.use(locationRoutes);
+
+//app.use((req, res, next) => {
+//  res.setHeader('Content-Type', 'text/html');
+//  next(); //goes to the next middleware, doesn't return a response for the request
+//});
+
+//app.use((req, res, next) => {
+//  const userName = req.body.username || 'Unknown User'; //req.body.username from the parsed body
+//  res.render('index', { //index file from views folder
+//    user: userName
+//  });
+//});
 
 app.listen(3000);
